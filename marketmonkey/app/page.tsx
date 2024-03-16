1321618ee1383c34c9062ff2836c2654eecf35c5
samuelandersondev <sanderson@redrocket.software>
2024-03-16 17:28:14 +0100

    feat: Add basic logo design

--- marketmonkey/app/page.tsx
+++ marketmonkey/app/page.tsx
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <main>
       <header>
-        <div>
-          <img src="logo.svg" alt="site logo" />
+        <div className="flex items-center">
+          <span className="text-16px m-0 p-0">Market</span>
+          <img src="logo.svg" alt="site logo" className="w-4 h-4 m-0 p-0 object-cover" />
+          <span className="text-16px m-0 p-0">Monkeys</span>
         </div>
       </header>
     </main>
